fea15b88d1c4c3ac077202ede20c88e3273251f8
MohammadReza <mohammadreza.etaati94@yahoo.com>
2022-07-04 07:55:59 +0430

    editfolders

--- inspector/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from inspector.views import CarFinished,AddPartCar
-
-urlpatterns = [
-    path('car-finished/<int:id>', CarFinished.as_view(), name='car-finished'),
-    path('car-add-par/<int:id>', AddPartCar.as_view(), name='car-add-part'),
-]
--- technician/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from technician.views import CarRepaired
-
-urlpatterns = [
-    path('car-repaired/<int:id>', CarRepaired.as_view(), name='car-repaired'),
-]
